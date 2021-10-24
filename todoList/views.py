b9486472d04c4e6454a6f6ebbf4fc08e4ddcfd16
fidifzination <fidifzination@gmail.com>
2021-10-24 19:01:16 +0530

    Display database items

--- todoList/views.py
+++ todoList/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from .models import TodoList
 # Create your views here.
 def index(request):
-    return render(request, 'todoList/index.html')
+    todo_items = TodoList.objects.order_by('id')
+    context = {'todo_items': todo_items}
+    return render(request, 'todoList/index.html', context) 
